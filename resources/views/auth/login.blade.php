a638c8e1ce3bd4cbc272779c85c9ff1ada77c3fa
Darkhan Mukatay <mukatay.darhan@gmail.com>
2017-03-31 00:45:13 +0600

    text labels changed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
                                 {{ csrf_field() }}
 
                                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                    <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                                    <label for="email" class="col-md-4 control-label">Login</label>
 
                                     <div class="col-md-6">
                                         <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -63,7 +63,7 @@
                                     <div class="col-md-6 col-md-offset-4">
                                         <div class="checkbox">
                                             <label>
-                                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Stay signed in
                                             </label>
                                         </div>
                                     </div>
@@ -72,7 +72,7 @@
                                 <div class="form-group">
                                     <div class="col-md-8 col-md-offset-4">
                                         <button type="submit" class="btn btn-primary">
-                                            Login
+                                            Sign in
                                         </button>
 
                                         <a class="btn btn-link" href="{{ route('password.request') }}">
